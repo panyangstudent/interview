c56b99af8ada411f89f6649dd3ffda8b0abda680
yanglei <yanglei@bilibili.com>
2021-03-28 21:04:48 +0800

    piupdaueg

---
+++ 两数相加/main.go
@@ -0,0 +1,41 @@
+package main
+
+struct ListNode{
+	Value int64
+	Next *ListNode
+}
+
+func main()  {
+	
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var carry int64
+	var sum int64
+	var newl1 *ListNode
+	var newl2 *ListNode
+	var respNode *ListNode
+	for {
+		temp := &ListNode{}
+		if l1.Next != nil {
+			
+			break
+		}
+		if l2.Next != nil {
+			break
+		}
+		newl1 = l1
+		newl2 = l2
+		sum = newl1.value + newl2.value
+		if sum >= 10 {
+			carry = 1
+			sum = sum % 10
+		} else {
+			carry = 0
+		}
+		temp.Value = sum
+		
+	}
+	return respNode
+}
+
