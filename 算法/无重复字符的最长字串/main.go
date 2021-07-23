a2605a2e9ae3d17dba230676765c8346edeb6e75
yanglei <yanglei@bilibili.com>
2021-07-23 19:28:54 +0800

    udpa

--- 算法/无重复字符的最长字串/main.go
+++ 算法/无重复字符的最长字串/main.go
@@ -1,9 +1,13 @@
 package main
 
+/*
+解题思路：
+1. 题目要求是无重复的字符的连续子串
+ */
 import "fmt"
 
 func main()  {
-	maxlen := lengthOfLongestSubstring("bbbbb")
+	maxlen := lengthOfLongestSubstring("pwwkew")
 	fmt.Println("maxlen is %v", maxlen)
 }
 func lengthOfLongestSubstring(s string) int {
@@ -14,15 +18,25 @@ func lengthOfLongestSubstring(s string) int {
 		existChar = make(map[rune]int)
 		maxLen = 0
 		tempMaxLen = 0
+		tempIndex = 0
 	)
 	for index, value := range []rune(s) {
-		if _,ok := existChar[value]; ok{
-			fmt.Println("maxlen is %v", value)
-			tempMaxLen = 1
-			existChar = make(map[rune]int)
+		if i ,ok := existChar[value]; ok{
+			if i <= tempIndex {
+
+			}
+			fmt.Println("maxlen is %v index : %v", value,index)
+
+			for key, value := range existChar {
+				if value <= i {
+					delete(existChar, key)
+					tempMaxLen--
+				}
+			}
 			existChar[value] = index
+			tempMaxLen++
 		} else {
-			fmt.Println("maxlen is %v", value)
+			fmt.Println("maxlen 111 is %v index : %v", value,index)
 			existChar[value] = index
 			tempMaxLen++
 			if tempMaxLen >= maxLen {
