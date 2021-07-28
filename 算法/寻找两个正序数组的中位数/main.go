317f6a691afcb368aafc8ef4cc2bf4e9778a56f5
yanglei <yanglei@bilibili.com>
2021-07-28 08:11:34 +0800

    upda

---
+++ 算法/寻找两个正序数组的中位数/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	findMedianSortedArrays()
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+
+	return 1.0
+}
--- 算法/无重复字符的最长字串/main.go
+++ 算法/无重复字符的最长字串/main.go
@@ -2,12 +2,16 @@ package main
 
 /*
 解题思路：
-1. 题目要求是无重复的字符的连续子串
+1. 题目要求是无重复的字符的连续子串,所以肯定是构造一个map，来查看这个map中是否存在，map的value存该字符的下标
+2. 然后foreach， 如果不存在之前的map中，就把当前的字符加入， 并且对应的最长字串长度加1
+3. 如果已经存在之前的map中了， 则删除之前已存在map中的小于当前下标的字符。这里的实现比较笨，有待后续优化，理论上只需要搞一个游走下标就可以了
  */
-import "fmt"
+import (
+	"fmt"
+)
 
 func main()  {
-	maxlen := lengthOfLongestSubstring("pwwkew")
+	maxlen := lengthOfLongestSubstring("aabaab!bb")
 	fmt.Println("maxlen is %v", maxlen)
 }
 func lengthOfLongestSubstring(s string) int {
@@ -18,15 +22,9 @@ func lengthOfLongestSubstring(s string) int {
 		existChar = make(map[rune]int)
 		maxLen = 0
 		tempMaxLen = 0
-		tempIndex = 0
 	)
 	for index, value := range []rune(s) {
 		if i ,ok := existChar[value]; ok{
-			if i <= tempIndex {
-
-			}
-			fmt.Println("maxlen is %v index : %v", value,index)
-
 			for key, value := range existChar {
 				if value <= i {
 					delete(existChar, key)
@@ -36,7 +34,6 @@ func lengthOfLongestSubstring(s string) int {
 			existChar[value] = index
 			tempMaxLen++
 		} else {
-			fmt.Println("maxlen 111 is %v index : %v", value,index)
 			existChar[value] = index
 			tempMaxLen++
 			if tempMaxLen >= maxLen {
