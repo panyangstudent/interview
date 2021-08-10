651b17d2ee53c6c52313dc943877bf9c754b8c8a
yanglei <yanglei@bilibili.com>
2021-08-10 10:02:37 +0800

    up[da

--- 算法/最长回文子串/main.go
+++ 算法/最长回文子串/main.go
@@ -1,9 +1,33 @@
 package main
 
 func main()  {
-	longestPalindrome("sssssdfsdfas")
+	// 暴力解法
+	longestPalindrome("发送到发生")
+	// 中心扩散法
+
+	// Manacher算法
 }
 
 func longestPalindrome(s string) string {
-	return "fsdf"
+	if len(s) < 2 {
+		return s
+	}
+	maxLongestPalindrome := s[:1]
+	for i:=0; i < len(s)-1; i++ {
+		for j := i+1; j < len(s); j++  {
+			if isPalindrome(s[i:j+1]) && j-i+1 > len(maxLongestPalindrome) {
+				maxLongestPalindrome = s[i:j+1]
+			}
+		}
+	}
+	return maxLongestPalindrome
+}
+
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s) - i -1] {
+			return false
+		}
+	}
+	return true
 }
