b883e9f8081cb68a32937cb4454839e8898f9dbe
yanglei <yanglei@bilibili.com>
2021-10-07 10:14:11 +0800

    upd

---
+++ Leetcode/环形链表/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 快慢指针
+func hasCycle(head *ListNode) bool {
+	if  head == nil || head.Next == nil {
+		return false
+	}
+	slow := head
+	fast := head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return false
+}
+
+
+// 哈希表
+
+func hasCycle(head *ListNode) bool {
+	if  head == nil || head.Next == nil {
+		return false
+	}
+	tempMap := make(map[*ListNode]bool)
+	for head != nil [
+		if _,ok := tempMap[head];ok {
+			return true
+		}
+		tempMap[head] = true
+		head = head.Next
+	]
+	return false
+}
