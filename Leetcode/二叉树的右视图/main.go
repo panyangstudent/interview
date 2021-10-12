fb49345becd1a0a0a992bf194b5626e712701e1a
yanglei <yanglei@bilibili.com>
2021-10-12 18:35:02 +0800

    udpa

---
+++ Leetcode/二叉树的右视图/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func rightSideView(root *TreeNode) []int {
+	res := make([]int, 0)
+    if root == nil {
+        return res
+    }
+	tempNodes := make([]*TreeNode, 0)
+	tempNodes = append(tempNodes, root)
+	for len(tempNodes) > 0 {
+		res  = append(res, tempNodes[len(tempNodes) -1 ].Val)
+		length := len(tempNodes)
+		for _, value := range tempNodes {
+			if value.Left != nil {
+				tempNodes = append(tempNodes, value.Left)
+			}
+			if value.Right != nil {
+				tempNodes = append(tempNodes, value.Right)
+			}
+		}
+		tempNodes = tempNodes[length:]
+	}
+	return res
+}
---
+++ Leetcode/岛屿最大的面积/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+func maxAreaOfIsland(grid [][]int) int {
+	m := len(grid)
+	max := 0
+	for i := 0;i< m;i++ {
+		for j := 0 ; j< grid[i];j++ {
+			if grid[i][j] == 1 {
+				tempMax := check(grid, i, j)
+				if max < tempMax  {
+					max = tempMax
+				}
+			}
+		}
+	}
+	return max
+}
+
+func check(grid [][]int, i, j int) int {
+	// 终止条件
+	if i < 0 || j < 0 || i >= len(grid) || j >=len(grid[i]) || grid[i][j] == 0 {
+		return 0
+	}
+	res := 1
+	grid[i][j] == 0
+	res += check(grid, i-1, j) 
+	res += check(grid, i+1, j) 
+	res += check(grid, i, j-1) 
+	res += check(grid, i, j+1) 
+	return res
+}
