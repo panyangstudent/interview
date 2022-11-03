84c880e9d8e8ccd400e8c63adaed710a169f9adf
yanglei <cooperyang@didiglobal.com>
2022-11-03 10:35:29 +0800

    uda

--- Leetcode/K个一组反转链表/main.go
+++ Leetcode/K个一组反转链表/main.go
@@ -1,7 +1,4 @@
 package main
-import (
-	"fmt"
-)
 
 
 type ListNode struct {
@@ -29,5 +26,28 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
         cur = tmp
     }
     stt.Next = reverseKGroup(cur, k)
+    return pre
+}
+
+func reverseKGroup1(head *ListNode, k int) *ListNode {
+    start, end := head, head
+    for i:=0;i<k;i++ {
+        if end == nil {
+            end = head
+            break
+        }
+        end = end.Next
+    }
+    if end == start {
+        return start
+    }
+    pre,cur := start, start
+    for cur != end {
+        temp := cur.Next
+        cur.Next = pre
+        pre = cur
+        cur = temp
+    }
+    start.Next = reverseKGroup(cur, k)
     return pre
 }
